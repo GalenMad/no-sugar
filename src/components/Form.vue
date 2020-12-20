516d20fd3d9c0c5f57fb5149a6e17779be59ba0f
sk0v0roda <steponist@mail.ru>
2020-12-20 19:42:48 +0300

    назвал акции акциями и добавил часть блока с формой

---
+++ src/components/Form.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <form>
+          <div class="form-group">
+            <label for="exampleInputname">Email address</label>
+            <input type="text" class="form-control" id="exampleInputname" />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPhone">Password</label>
+            <input type="phone" class="form-control" id="exampleInputPhone" />
+            <small id="phoneHelp" class="form-text text-muted"
+              >Мы не будем рассылать вам спам или рекламные предложения.</small
+            >
+          </div>
+          <div class="form-group form-check">
+            <input
+              type="checkbox"
+              class="form-check-input"
+              id="exampleCheck1"
+            />
+            <label class="form-check-label" for="exampleCheck1"
+              >Я согласен на обработку персональных данных</label
+            >
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
---
+++ src/components/Promo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-4" v-for="item in catalog" :key="item.index">
+          <div class="card" style="width: 18rem">
+            <img :src="item.img" class="card-img-top" :alt="item.name" />
+            <div class="card-body">
+              <h5 class="card-title">{{ item.name }}</h5>
+              <p class="card-text">
+                {{ item.description }}
+              </p>
+              <a href="#" class="btn btn-primary">Купить</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Catalog from '../App.vue'
+export default {
+  catalog: Catalog
+}
+</script>
