36de7a2ae95d2d76272201b6c277574403f3aff0
sk0v0roda <steponist@mail.ru>
2020-12-21 22:32:28 +0300

    Сделал блок с формой

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,31 +1,35 @@
 <template>
   <div>
+    <h2>Напишите нам и мы вам перезвоним !</h2>
     <div class="container">
       <div class="row">
-        <form>
-          <div class="form-group">
-            <label for="exampleInputname">Email address</label>
-            <input type="text" class="form-control" id="exampleInputname" />
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPhone">Password</label>
-            <input type="phone" class="form-control" id="exampleInputPhone" />
-            <small id="phoneHelp" class="form-text text-muted"
-              >Мы не будем рассылать вам спам или рекламные предложения.</small
-            >
-          </div>
-          <div class="form-group form-check">
-            <input
-              type="checkbox"
-              class="form-check-input"
-              id="exampleCheck1"
-            />
-            <label class="form-check-label" for="exampleCheck1"
-              >Я согласен на обработку персональных данных</label
-            >
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        <div class="card">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputName">Ваше имя</label>
+              <input type="text" class="form-control" id="exampleInputName" />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPhone">Телефон</label>
+              <input type="tel" class="form-control" id="exampleInputTel" />
+              <small id="telHelp" class="form-text text-muted"
+                >Мы не будем рассылать вам спам или рекламные
+                предложения.</small
+              >
+            </div>
+            <div class="form-group form-check">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                id="exampleCheck1"
+              />
+              <label class="form-check-label" for="exampleCheck1"
+                >Я согласен на обработку персональных данных</label
+              >
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
       </div>
     </div>
   </div>
@@ -33,3 +37,27 @@
 <script>
 export default {}
 </script>
+<style scoped>
+h2 {
+  margin-top: 50px;
+  text-align: center;
+}
+.form-group {
+  width: 30%;
+  margin-right: auto;
+  margin-left: auto;
+}
+.card {
+  width: 90%;
+  margin-right: auto;
+  margin-left: auto;
+  display: flex;
+  min-height: 300px;
+  margin-bottom: 30px;
+  justify-content: space-around;
+}
+.btn {
+  width: 20%
+}
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <app-promo/>
+    <app-form/>
   </div>
 </template>
 <script>
 import Promo from '../components/Promo.vue'
+import Form from '../components/Form.vue'
 export default {
   props: Array,
   components: {
-    appPromo: Promo
+    appPromo: Promo,
+    appForm: Form
   }
 }
 </script>
